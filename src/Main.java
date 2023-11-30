8a8f1891833818c4bba31b3fbd107aa4406bc663
bigwati <jachymek.kuba@gmail.com>
2023-11-30 14:03:02 +0100

    my_changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
-
+    public static void teaTime() {
+        System.out.println("Waiting for tea time...");
+        System.out.println("Type and enter to start tea time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's tea time");
+    }
     public static void main(String[] args) {
-
+        System.out.println("welcome to new job");
+        teaTime();
+        System.out.println("write code");
+        System.out.println("review code");
+        teaTime();
+        System.out.println("promotion");
     }
 
 }
