528a2352ea7f85f204ed1528d8c38d42cb388fe5
bigwati <jachymek.kuba@gmail.com>
2023-11-30 14:09:57 +0100

    my_changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double mealPrice, double tipRate, double taxRate) {
+        double tip = tipRate*mealPrice;
+        double tax = taxRate*mealPrice;
+        double result = mealPrice+tip+tax;
+        System.out.println("total meal price is "+result);
+
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(15,0.15,0.08);
+        calculateTotalMealPrice(25,0.18,0.08);
     }
 
 }
