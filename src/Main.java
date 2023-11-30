2400107f5aaf275f962f286a053921697801a46c
bigwati <jachymek.kuba@gmail.com>
2023-11-30 14:46:54 +0100

    my_changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
+    public static double salary(double workedHours,double hourSalary){
+        return workedHours*hourSalary;
+    }
+    public static double vacationDays(double workedHours){
+        return Math.ceil(workedHours/8);
+    }
+
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Whats is your hour salary?");
+        double hourSalary = input.nextDouble();
+        System.out.println("How many hours did you work?");
+        double workedHours = input.nextDouble();
+
+        System.out.println("Your salary is: "+salary(workedHours,hourSalary));
+        System.out.println("You have " + vacationDays(workedHours) + " vacation days");
     }
 
 }
